befcb5e179731c935ff9be73b8487ac01f587ad4
Swapnil Bandgar <52813814+swapyGitCode@users.noreply.github.com>
2021-10-15 18:16:39 -0400

    make it better

--- app.py
+++ app.py
@@ -4,12 +4,12 @@
 # In[11]:
 
 
-import numpy as np
+# import numpy as np
 import pandas as pd
 import flask as Flask
 from flask import request
 from flask import render_template
-import pickle
+# import pickle
 
 
 # In[12]:
@@ -38,107 +38,23 @@ def home():
 # In[16]:
 
 
-@app.route('/linkedin', methods = ['GET'])
-def linkedin():
-    # if request.method == 'POST':
-    #     to_predict_list = request.form.to_dict()
-    #     to_predict_list = list(to_predict_list.values())
-    #     if to_predict_list[0] == 'Male':
-    #         to_predict_list[0] = 0
-    #     else:
-    #         to_predict_list[0] = 1
-    #     to_predict_list = list(map(float, to_predict_list))
-    #     result = ValuePredictor(to_predict_list)
-    #     if int(result) == 0:
-    #         prediction = 'You are in good shape!!!'
-    #     else:
-    #         prediction = 'Please, start working out'
-        return render_template("linkedin.html")
+# @app.route('/linkedin', methods = ['GET'])
+# def linkedin():
+#     # if request.method == 'POST':
+#     #     to_predict_list = request.form.to_dict()
+#     #     to_predict_list = list(to_predict_list.values())
+#     #     if to_predict_list[0] == 'Male':
+#     #         to_predict_list[0] = 0
+#     #     else:
+#     #         to_predict_list[0] = 1
+#     #     to_predict_list = list(map(float, to_predict_list))
+#     #     result = ValuePredictor(to_predict_list)
+#     #     if int(result) == 0:
+#     #         prediction = 'You are in good shape!!!'
+#     #     else:
+#     #         prediction = 'Please, start working out'
+#         return render_template("linkedin.html")
 
-@app.route('/github', methods = ['GET'])
-def github():
-    # if request.method == 'POST':
-    #     to_predict_list = request.form.to_dict()
-    #     to_predict_list = list(to_predict_list.values())
-    #     if to_predict_list[0] == 'Male':
-    #         to_predict_list[0] = 0
-    #     else:
-    #         to_predict_list[0] = 1
-    #     to_predict_list = list(map(float, to_predict_list))
-    #     result = ValuePredictor(to_predict_list)
-    #     if int(result) == 0:
-    #         prediction = 'You are in good shape!!!'
-    #     else:
-    #         prediction = 'Please, start working out'
-        return render_template("github.html")
-
-@app.route('/medium', methods = ['GET'])
-def medium():
-    # if request.method == 'POST':
-    #     to_predict_list = request.form.to_dict()
-    #     to_predict_list = list(to_predict_list.values())
-    #     if to_predict_list[0] == 'Male':
-    #         to_predict_list[0] = 0
-    #     else:
-    #         to_predict_list[0] = 1
-    #     to_predict_list = list(map(float, to_predict_list))
-    #     result = ValuePredictor(to_predict_list)
-    #     if int(result) == 0:
-    #         prediction = 'You are in good shape!!!'
-    #     else:
-    #         prediction = 'Please, start working out'
-        return render_template("medium.html")
-        
-@app.route('/instagram', methods = ['GET'])
-def instagram():
-    # if request.method == 'POST':
-    #     to_predict_list = request.form.to_dict()
-    #     to_predict_list = list(to_predict_list.values())
-    #     if to_predict_list[0] == 'Male':
-    #         to_predict_list[0] = 0
-    #     else:
-    #         to_predict_list[0] = 1
-    #     to_predict_list = list(map(float, to_predict_list))
-    #     result = ValuePredictor(to_predict_list)
-    #     if int(result) == 0:
-    #         prediction = 'You are in good shape!!!'
-    #     else:
-    #         prediction = 'Please, start working out'
-        return render_template("instagram.html")
-
-@app.route('/facebook', methods = ['GET'])
-def facebook():
-    # if request.method == 'POST':
-    #     to_predict_list = request.form.to_dict()
-    #     to_predict_list = list(to_predict_list.values())
-    #     if to_predict_list[0] == 'Male':
-    #         to_predict_list[0] = 0
-    #     else:
-    #         to_predict_list[0] = 1
-    #     to_predict_list = list(map(float, to_predict_list))
-    #     result = ValuePredictor(to_predict_list)
-    #     if int(result) == 0:
-    #         prediction = 'You are in good shape!!!'
-    #     else:
-    #         prediction = 'Please, start working out'
-        return render_template("facebook.html")
-
-@app.route('/twitter', methods = ['GET'])
-def twitter():
-    # if request.method == 'POST':
-    #     to_predict_list = request.form.to_dict()
-    #     to_predict_list = list(to_predict_list.values())
-    #     if to_predict_list[0] == 'Male':
-    #         to_predict_list[0] = 0
-    #     else:
-    #         to_predict_list[0] = 1
-    #     to_predict_list = list(map(float, to_predict_list))
-    #     result = ValuePredictor(to_predict_list)
-    #     if int(result) == 0:
-    #         prediction = 'You are in good shape!!!'
-    #     else:
-    #         prediction = 'Please, start working out'
-        return render_template("twitter.html")
 
 # In[17]:
 
